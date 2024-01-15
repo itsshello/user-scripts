c97946794cc65d568fb2cf5caa086b29a67e47ee
Hello <149557798+itsshello@users.noreply.github.com>
2024-01-15 22:17:35 +0600

    made a script

---
+++ js.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+const body = document.querySelector("body");
+
+body.innerHTML = " ";
+
+body.innerHTML = `  
+
+    <div>
+        <p> Masud you can not write java script <p>
+    </div>
+
+    <h1>ASHFAQ</h1>
+
+`;
+
+};
